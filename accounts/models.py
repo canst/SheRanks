218c4a76d8aca9976f6d5b4ecb0edcbe37840fed
canst <47247401+canst@users.noreply.github.com>
2025-07-31 21:33:52 +0100

    u

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    # Add other fields here as needed, e.g., profile picture, university affiliation, etc.
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
